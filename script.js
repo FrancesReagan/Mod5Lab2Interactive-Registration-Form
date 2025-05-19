63d23f21f1853c9d9270f0aec6c0821413d3f0be
Frances Reagan James <adtgwen@gmail.com>
2025-05-19 12:01:55 -0400

    added form submission, validation checks all fields, added success message

--- script.js
+++ script.js
@@ -45,8 +45,57 @@ form.addEventListener("submit", function(event) {
     successMessage.textContent = "Registration successful!!";
     successMessage.classList.add("visible");
 
-    
-  }
-}
+    //can hid the success message after 3 seconds//
+    setTimeout(() => {
+      successMessage.classList.remove("visible");
+    }, 3000);
 
-)
+    //Optional: Reset the form (except username)//
+    email.value = "";
+    password.value = "";
+    confirmPassword.value = "";
+
+    //Reset classes//
+    email.classList.remove("valid");
+    password.classList.remove("valid");
+    confirmPassword.classList.remove("valid"); 
+    } else {
+      //Focus on the first invalid field//
+      if (!isUsernameValid) {
+        username.focus();
+      } else if (!isEmailValid) {
+        email.focus();
+      } else if (!isPasswordValid) {
+        password.focus();
+      } else {
+        confirmPassword.focus();
+      }
+    }
+  });
+
+  //Validation functions
+  function validateUsername() {
+    //Clear previous error message
+    usernameError.textContent = "";
+
+    //Check if username is empty//
+    if (username.value.trim() === "") {
+      username.Error.textContent = "Username is required";
+      username.classList.remove("valid");
+      username.classList.add("invalid");
+      return false;
+    }
+
+    //Check if username is at least 3 characters//
+    if (username.value.length < 3) {
+      username.Error.textContent = "Username must be atleast 3 characters";
+      username.classList.remove("valid");
+      username.classList.add("invalid");
+      return true;
+    }
+
+    function validateEmail() {
+      //Clear previous error message//
+      email
+    }
+  }
